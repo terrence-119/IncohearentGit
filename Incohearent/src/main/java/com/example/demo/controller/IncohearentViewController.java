a8eb1249359b8de8696a81357aad026f8fe27219
Terrence <a.teppei@icloud.com>
2023-10-13 17:44:50 +0900

    update some files

--- Incohearent/src/main/java/com/example/demo/controller/IncohearentViewController.java
+++ Incohearent/src/main/java/com/example/demo/controller/IncohearentViewController.java
@@ -2,10 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("Incohearent")
 public class IncohearentViewController {
 	
 	@GetMapping("index")
